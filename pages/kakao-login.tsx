17aaf17ff9001eebadf26011436d6a64c1f0a4c3
yunchang.choi <ycc4477@gmail.com>
2021-10-24 19:17:48 +0900

    fix: javascript

--- pages/kakao-login.tsx
+++ pages/kakao-login.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 
 export default function KakaoLogin() {
   const router = useRouter();
-  const REST_API_KEY = '1615fdafcfab4319323f1bb7015df0e4';
+  const REST_API_KEY = '1fa3d84c220e7a4cbc19ac98ad079f9a';
   // const REST_API_KEY = '1615fdafcfab4319323f1bb7015df0e4';
   const REDIRECT_URI = 'http://52.78.71.182:3000/login';
   const REQUEST_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
