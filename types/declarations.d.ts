6afcf870d1febd5e5d0f46c200b1b74e03457d7b
eggplantiny <eggplantiny@gmail.com>
2021-10-30 04:26:59 +0900

    klipSDK bug fix

--- types/declarations.d.ts
+++ types/declarations.d.ts
@@ -1,6 +1,15 @@
+
 declare module '*.svg' {
   import React from 'react';
 
   const content: React.FC<SvgProps>;
   export default content;
 }
+
+declare global {
+  interface Window {
+    klipSDK: any;
+  }
+}
+
+let klipSDK = window.klipSDK;
